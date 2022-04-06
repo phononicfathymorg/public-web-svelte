82cca3329dd719fac990dc4d08d0a3c2bcc2a0eb
Jeremy Tomlinson <jeremy.tomlinson@gmail.com>
2022-04-05 20:03:49 -0600

    Updates to embed power bi report

--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,13 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<iframe title="powerfleet-LV450-reefer-tracking" width="1540" height="941.25" src="https://app.powerbi.com/reportEmbed?reportId=fb3f43ae-c4ac-4f17-8432-9f3165184139&groupId=5289e89f-38bb-49db-a4d2-dda0678fd390&autoAuth=true&ctid=6dcbebd0-f8d0-4a9d-89e5-5873e8146b0a&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWNlbnRyYWwtdXMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="0" allowFullScreen="true"></iframe>
 </main>
 
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
+		padding: 0.5em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
